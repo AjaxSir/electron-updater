f806662ee1ce6fb4baba661172726004ffc5d38b
suxiaolong <nevesto777777@gmail.com>
2026-01-06 14:35:49 +0800

    fix: 新增触发更新机制

--- electron/main.js
+++ electron/main.js
@@ -1,12 +1,14 @@
 /*
  * @Date: 2026-01-05 15:34:43
  * @LastEditors: xiaolong.su
- * @LastEditTime: 2026-01-05 17:12:52
+ * @LastEditTime: 2026-01-06 14:33:28
  * @Description:
  */
 const path = require("path");
 const url = require("url");
 const { app, BrowserWindow, ipcMain } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const { dialog } = require("electron");
 let mainWindow = null;
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -45,4 +47,46 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
-app.on("ready", createWindow);
+app.on("ready", () => {
+  console.log("是否打包：", app.isPackaged);
+  console.log("当前版本：", app.getVersion());
+  console.log("更新源：", autoUpdater.getFeedURL?.());
+  createWindow();
+  initAutoUpdate(mainWindow);
+  autoUpdater.checkForUpdates();
+});
+const initAutoUpdate = win => {
+  autoUpdater.on("checking-for-update", () => {
+    console.log("检查更新中");
+  });
+
+  autoUpdater.on("update-available", info => {
+    console.log("发现新版本", info);
+  });
+
+  autoUpdater.on("update-not-available", () => {
+    console.log("当前是最新版本");
+  });
+
+  autoUpdater.on("download-progress", progress => {
+    win.webContents.send("update-progress", progress);
+  });
+
+  autoUpdater.on("update-downloaded", () => {
+    dialog
+      .showMessageBox(win, {
+        type: "info",
+        buttons: ["立即更新", "稍后"],
+        message: "新版本已下载，是否立即安装？"
+      })
+      .then(res => {
+        if (res.response === 0) {
+          autoUpdater.quitAndInstall();
+        }
+      });
+  });
+
+  autoUpdater.on("error", err => {
+    console.error("更新失败", err);
+  });
+};
